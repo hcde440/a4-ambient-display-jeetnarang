db0a9665d80d82c8f545275477b17a6eb37ee5ff
Japjeet Singh Narang <jsn96@uw.edu>
2019-05-09 09:01:16 -0700

    Final changes

---
+++ Ambient Display/Display_Platform/Display_Platform.ino
@@ -0,0 +1,131 @@
+
+
+#include <ESP8266WiFi.h> //Requisite Libraries . . .
+#include "Wire.h"           //
+#include <PubSubClient.h>   //
+#include <ArduinoJson.h>    /
+
+#define wifi_ssid "Krusty Krab"   //You have seen this before
+#define wifi_password "Ziggystardust" //
+
+
+//////////
+//So to clarify, we are connecting to and MQTT server
+//that has a login and password authentication
+//I hope you remember the user and password
+//////////
+
+#define mqtt_server "mediatedspaces.net"  //this is its address, unique to the server
+#define mqtt_user "hcdeiot"               //this is its server login, unique to the server
+#define mqtt_password "esp8266"           //this is it server password, unique to the server
+
+//////////
+//We also need to publish and subscribe to topics, for this sketch are going
+//to adopt a topic/subtopic addressing scheme: topic/subtopic
+//////////
+
+WiFiClient espClient;             //blah blah blah, espClient
+PubSubClient mqtt(espClient);     //blah blah blah, tie PubSub (mqtt) client to WiFi client
+
+//////////
+//We need a 'truly' unique client ID for our esp8266, all client names on the server must be unique.
+//Every device, app, other MQTT server, etc that connects to an MQTT server must have a unique client ID.
+//This is the only way the server can keep every device separate and deal with them as individual devices/apps.
+//The client ID is unique to the device.
+//////////
+
+char mac[6]; //A MAC address is a 'truly' unique ID for each device, lets use that as our 'truly' unique user ID!!!
+
+//////////
+//In our loop(), we are going to create a c-string that will be our message to the MQTT server, we will
+//be generous and give ourselves 200 characters in our array, if we need more, just change this number
+//////////
+
+char message[201]; //201, as last character in the array is the NULL character, denoting the end of the array
+
+unsigned long currentMillis, timerOne, timerTwo, timerThree; //we are using these to hold the values of our timers
+
+/////SETUP/////
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  pinMode(2, OUTPUT);
+  mqtt.setServer(mqtt_server, 1883);
+  mqtt.setCallback(callback); //register the callback function
+  timerOne = timerTwo = timerThree = millis();
+}
+
+
+/////SETUP_WIFI/////
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+  WiFi.begin(wifi_ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");  //get the unique MAC address to use as MQTT client ID, a 'truly' unique ID.
+  Serial.println(WiFi.macAddress());  //.macAddress returns a byte array 6 bytes representing the MAC address
+}                                     //5C:CF:7F:F0:B0:C1 for example
+
+/////CONNECT/RECONNECT/////Monitor the connection to MQTT server, if down, reconnect
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqtt.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (mqtt.connect(mac, mqtt_user, mqtt_password)) { //<<---using MAC as client ID, always unique!!!
+      Serial.println("connected");
+      mqtt.subscribe("jay/displayState"); //we are subscribing to 'theTopic' and all subtopics below that topic
+    } else {                        //please change 'theTopic' to reflect your topic you are subscribing to
+      Serial.print("failed, rc=");
+      Serial.print(mqtt.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+// the loop function runs over and over again forever
+void loop() {
+  if (!mqtt.connected()) {
+    reconnect();
+  }
+
+  mqtt.loop(); //this keeps the mqtt connection 'active'
+
+}
+
+
+/////CALLBACK/////
+//The callback is where we attach a listener to the incoming messages from the server.
+//By subscribing to a specific channel or topic, we can listen to those topics we wish to hear.
+//We place the callback in a separate tab so we can edit it easier . . . (will not appear in separate
+//tab on github!)
+/////
+const byte led = 2;
+char message_buff[100];
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received: ");
+  Serial.println(topic);
+
+  Serial.print("payload: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  /* we got '1' -> on */
+  if ((char)payload[0] == '1') {
+    digitalWrite(led, HIGH); 
+  } else {
+    /* we got '0' -> on */
+    digitalWrite(led, LOW);
+  }
+}
